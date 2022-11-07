b70747a0243092fcd190e9495daaeffdb1c350b7
Luigi Matias <luigi.matias@codehunter.com>
2022-11-07 10:49:44 -0500

    added fetch the data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { sendCartData } from './store/cart-slice'; 
+import { fetchCartData, sendCartData } from './store/cart-actions'; 
 import Notification from './components/UI/Notification';
 
 let isInitial = true;
@@ -14,6 +14,10 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {   
     if (isInitial) {
